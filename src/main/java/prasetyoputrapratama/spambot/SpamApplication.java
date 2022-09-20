defe9c68bd6de3f3d7e73c838713700a14af75fa
PrasGi <prastyopratama01@gmail.com>
2022-09-20 21:21:18 +0700

    first push

---
+++ src/main/java/prasetyoputrapratama/spambot/SpamApplication.java
@@ -0,0 +1,13 @@
+package prasetyoputrapratama.spambot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpamApplication.class, args);
+	}
+
+}
---
+++ src/test/java/prasetyoputrapratama/spambot/SpamApplicationTests.java
@@ -0,0 +1,13 @@
+package prasetyoputrapratama.spambot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
